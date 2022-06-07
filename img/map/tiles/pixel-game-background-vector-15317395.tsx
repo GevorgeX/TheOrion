44f5be22fb7ecd8e8f2836ca3a1061ff51929427
GevorgeX <gevnavoyan@gmail.com>
2022-06-07 12:00:39 +0400

    Version 1.3

---
+++ img/map/tiles/pixel-game-background-vector-15317395.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="pixel-game-background-vector-15317395" tilewidth="32" tileheight="32" tilecount="775" columns="31">
+ <image source="../pixel-game-background-vector-15317395.jpg" width="1000" height="830"/>
+</tileset>
